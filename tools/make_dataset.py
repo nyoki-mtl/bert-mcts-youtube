e210904852b84fb0f97affef1294e5c926fcaca0
Tomohide Shibata <tomohide.shibata@gmail.com>
2021-05-22 13:06:07 +0000

    --test_sizeオプションを追加

--- tools/make_dataset.py
+++ tools/make_dataset.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 from pathlib import Path
 
 import numpy as np
@@ -6,8 +7,14 @@ from sklearn.model_selection import train_test_split
 from src.utils.hcpe import get_data_from_hcpe, load_hcpes
 from src.utils.sfen import get_data_from_sfen, get_gokaku_sfen_paths, load_sfens
 
+def argparse():
+    parser = ArgumentParser(description='Make dataset')
+    parser.add_argument('--test_size', type=float, default=0.1)
+    args, _ = parser.parse_known_args()
+    return args
 
-def main():
+
+def main(args):
     gokaku_dir = Path('./data/ShogiGokakuKyokumen/')
     hcpe_dir = Path('./data/hcpe/')
     dataset_base_dir = Path('./data/dataset/')
@@ -17,7 +24,7 @@ def main():
         sfen_paths = get_gokaku_sfen_paths(gokaku_dir, num_of_moves)
         sfens = load_sfens(sfen_paths)
         data = get_data_from_sfen(sfens)
-        train_data, valid_data = train_test_split(data, test_size=0.2, random_state=42)
+        train_data, valid_data = train_test_split(data, test_size=args.test_size, random_state=42)
 
         dataset_dir = dataset_base_dir / f'gokaku_{num_of_moves:03d}'
         dataset_dir.mkdir()
@@ -30,7 +37,7 @@ def main():
     hcpes = load_hcpes(hcpe_paths)
     data = get_data_from_hcpe(hcpes)
 
-    train_data, valid_data = train_test_split(data, test_size=0.2, random_state=42)
+    train_data, valid_data = train_test_split(data, test_size=args.test_size, random_state=42)
 
     dataset_dir = dataset_base_dir / 'selfplay'
     dataset_dir.mkdir()
@@ -40,4 +47,5 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    args = argparse()
+    main(args)
